2a9d41a8ae43b0a7454c8744fe1479490ba11901
Alexander Balchev <abalchev2@dxc.com>
2021-03-21 16:15:17 +0200

    Initial commit

---
+++ mtg_spoilers.py
@@ -0,0 +1,29 @@
+import datetime
+import re
+import time
+import webbrowser
+
+import praw
+from win10toast import ToastNotifier
+
+from config import config
+
+REDDIT = praw.Reddit(client_id=config['CLIENT_ID'],
+                     client_secret=config['CLIENT_SECRET'],
+                     user_agent=config["USER_AGENT"])
+BROWSER_PATH = config['BROWSER_PATH']
+SUBREDDIT = config['SUBREDDIT']
+posts = []
+posts_names = []
+toaster = ToastNotifier()
+
+if __name__ == "__main__":
+    while True:
+        for submission in REDDIT.subreddit(SUBREDDIT).new(limit=10):
+            if (submission.link_flair_text == config['SPOILER_FLAIR'] or re.match(r'\[([^\]]*)\]', submission.title)) and submission.title[6:].lower() not in posts_names:
+                posts_names.append(submission.title[6:].lower())
+                if not submission.is_self:
+                    toaster.show_toast("New Spoiler", submission.title)
+                    webbrowser.get(CHROME_PATH).open(
+                        f'https://www.reddit.com{submission.permalink}')
+        time.sleep(60)
